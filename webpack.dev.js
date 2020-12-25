76c60fdc8b387f9d69bcd87534444faa16b15842
lukegibran <liukjhivran@gmail.com>
2020-12-26 01:25:32 +0800

    Fixed issue with webpack dev command

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,14 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     historyApiFallback: true,
     proxy: {
-      "/api": "http://localhost:5001",
+      '/api': 'http://localhost:5001',
     },
     open: true,
     compress: true,
